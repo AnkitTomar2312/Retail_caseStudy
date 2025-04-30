e01bb357e670ce8d976ca1f9671da4ce50b2c216
Ankit Tomar <ankitjdec@gmail.com>
2025-04-30 21:12:49 +0530

    founded the dupplicate 4999 and 5000

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,2 +1,8 @@
 create database retail;
 use retail;
+
+ -- Removing duplicate transcations
+ select TransactionID, count(*) as row_count
+ from sales_transaction
+ group by TransactionID
+ having row_count>1;
