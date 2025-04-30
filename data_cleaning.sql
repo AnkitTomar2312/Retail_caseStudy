acc6039cb51a5e4ca642f265aebb0e5d5b704a17
Ankit Tomar <ankitjdec@gmail.com>
2025-04-30 21:30:51 +0530

    dropped the sales_transaction table

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -6,3 +6,18 @@ use retail;
  from sales_transaction
  group by TransactionID
  having row_count>1;
+ 
+ -- selecting distinct entries and creating a new table
+ 
+create table sales_transaction_temp as select distinct * from sales_transaction;
+
+-- checking for the duplicate values
+select TransactionID, count(*) as row_count
+ from sales_transaction_temp
+ group by TransactionID
+ having row_count>1;
+ 
+-- deleting the sales_transaction table
+
+drop table if exists sales_transaction;
+ 
