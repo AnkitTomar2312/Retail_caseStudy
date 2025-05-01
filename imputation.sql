9a42269bf7ec7923e62d18d0b3e50608e4c9eb79
Ankit Tomar <ankitjdec@gmail.com>
2025-05-01 19:31:08 +0530

    -- imputed mode(west) in the location attribute of the customer table

--- imputation.sql
+++ imputation.sql
@@ -3,3 +3,21 @@ use retail;
 select * from customer;
 
 -- imputing mode in the location attribute of the customer table
+savepoint bef_imp;
+drop table customer_temp;
+create table customer_temp (select * from customer);
+
+update customer
+set
+Location=(select Location
+from customer_temp
+group by Location
+order by count(*) desc
+limit 1
+)
+where Location='Unknown';
+
+select *
+from customer
+where location='Unknown';
+
