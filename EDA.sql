9c5e7a85b136f6b7029f1c67c837dea0871c5372
Ankit Tomar <ankitjdec@gmail.com>
2025-05-01 19:40:19 +0530

    prob1 finished

--- EDA.sql
+++ EDA.sql
@@ -2,3 +2,14 @@
 
 -- Get a summary of total sales and quantities sold per product.
 -- Write a SQL query to summarize the total sales and quantities sold per product by the company.
+
+select
+p.ProductID,
+round(sum(t.QuantityPurchased*t.Price),2) as total_sales,
+sum(t.QuantityPurchased) as Toatal_Quantity
+from product_inventory p
+inner join
+sales_transaction t
+on p.ProductID=t.ProductID
+group by p.ProductID;
+
