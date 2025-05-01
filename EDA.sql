19eab1384f7398ac7ebf6904579fd1bd09b40f73
Ankit Tomar <ankitjdec@gmail.com>
2025-05-01 19:34:49 +0530

    eda part begin

---
+++ EDA.sql
@@ -0,0 +1,4 @@
+-- Exploratory Data Analysis (EDA)
+
+-- Get a summary of total sales and quantities sold per product.
+-- Write a SQL query to summarize the total sales and quantities sold per product by the company.
